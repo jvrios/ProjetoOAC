1a10a0952f3b017e6f62dc5f3ded80ce6cde8f2d
jvrios <67750927+jvrios@users.noreply.github.com>
2023-10-23 01:30:03 -0300

    Update jogodavelha_1.asm

--- jogodavelha_1.asm
+++ jogodavelha_1.asm
@@ -2,8 +2,8 @@
 coluna: .asciiz "|"
 linha: .asciiz "---+---+---\n"
 newline: .asciiz "\n"
-X: .asciiz " X "
 O: .asciiz " O "
+X: .asciiz " X "
 espaco: .asciiz "   "
 return1: .word 0		#recebe 0(espaco) 1 (X) ou 2 (O)
 return2: .word 0		#recebe 0(espaco) 1 (X) ou 2 (O)
@@ -14,60 +14,107 @@ return6: .word 0		#recebe 0(espaco) 1 (X) ou 2 (O)
 return7: .word 0		#recebe 0(espaco) 1 (X) ou 2 (O)
 return8: .word 0		#recebe 0(espaco) 1 (X) ou 2 (O)
 return9: .word 0		#recebe 0(espaco) 1 (X) ou 2 (O)
-board: .space 9        # 3x3 board
 turno: .word 0
-player1: .asciiz "Player 1's turn\n"
-player2: .asciiz "Player 2's turn\n"
 invalido: .asciiz "Movimento inválido\n"
-player_prompt: .asciiz "Enter a number (1-9): "
+player_prompt: .asciiz "Entre com um numero (1-9): "
+vitoria1_prompt: .asciiz "Vitoria jogador 1 :p"
+vitoria2_prompt: .asciiz "Vitoria jogador 2 :p"
+empate_prompt: .asciiz "Empate :("
 .text
-main:
     
-pos1:
+ main:
     li $t4, 1
-    lw $t0, return1
-    li $t1, 1
-    beq $t0, $t1, is_one
-    li $t2, 2
-    beq $t0, $t2, is_two
+    lw $t0, turno
+    move $t0, $t4
+pos1:
+    lw $a0, turno
 
-    # If the input value is neither 1 nor 2, print " "
-    la $a0, espaco
+    li $v0, 1
+    syscall
+
+    la $a0, newline
     li $v0, 4
     syscall
-    j pos2
+    lw $t0, return1
+    li $t1, 1
+    beq $t0, $t1, printOne1
+
+    li $t2, 2
+    beq $t0, $t2, printTwo1
+
+    j printEspaco1
+
+    printOne1:
+        jal is_one
+        j continuar1
+
+    printTwo1:
+        jal is_two
+        j continuar1
+
+    printEspaco1:
+        la $a0, espaco
+        li $v0, 4
+        syscall
+
+    continuar1:
 pos2:
-    li $t4, 2
     la $a0, coluna
     li $v0, 4
     syscall
     lw $t0, return2
-    li $t1, 1
-    beq $t0, $t1, is_one
-    li $t2, 2
-    beq $t0, $t2, is_two
 
-    la $a0, espaco
-    li $v0, 4
-    syscall
-    j pos3
+    li $t1, 1
+    beq $t0, $t1, printOne2
+
+    li $t2, 2
+    beq $t0, $t2, printTwo2
+
+    j printEspaco2
+
+    printOne2:
+        jal is_one
+        j continuar2
+
+    printTwo2:
+        jal is_two
+        j continuar2
+
+    printEspaco2:
+        la $a0, espaco
+        li $v0, 4
+        syscall
+
+    continuar2:
+
 pos3:
-    li $t4, 3
     la $a0, coluna
     li $v0, 4
     syscall
     lw $t0, return3
     li $t1, 1
-    beq $t0, $t1, is_one
-    li $t2, 2
-    beq $t0, $t2, is_two
+    beq $t0, $t1, printOne3
 
-    la $a0, espaco
-    li $v0, 4
-    syscall
-    j pos4
+    li $t2, 2
+    beq $t0, $t2, printTwo3
+
+    j printEspaco3
+
+    printOne3:
+        jal is_one
+        j continuar3
+
+    printTwo3:
+        jal is_two
+        j continuar3
+
+    printEspaco3:
+        la $a0, espaco
+        li $v0, 4
+        syscall
+
+    continuar3:
 pos4:
-    li $t4, 4
     la $a0, newline
     li $v0, 4
     syscall
@@ -75,46 +122,82 @@ pos4:
     syscall
     lw $t0, return4
     li $t1, 1
-    beq $t0, $t1, is_one
-    li $t2, 2
-    beq $t0, $t2, is_two
+    beq $t0, $t1, printOne4
 
-    la $a0, espaco
-    li $v0, 4
-    syscall
-    j pos5
+    li $t2, 2
+    beq $t0, $t2, printTwo4
+
+    j printEspaco4
+
+    printOne4:
+        jal is_one
+        j continuar4
+
+    printTwo4:
+        jal is_two
+        j continuar4
+
+    printEspaco4:
+        la $a0, espaco
+        li $v0, 4
+        syscall
+
+    continuar4:
 pos5:
-    li $t4, 5
     la $a0, coluna
     li $v0, 4
     syscall
     lw $t0, return5
     li $t1, 1
-    beq $t0, $t1, is_one
-    li $t2, 2
-    beq $t0, $t2, is_two
+    beq $t0, $t1, printOne5
 
-    la $a0, espaco
-    li $v0, 4
-    syscall
-    j pos6
+    li $t2, 2
+    beq $t0, $t2, printTwo5
+
+    j printEspaco5
+
+    printOne5:
+        jal is_one
+        j continuar5
+
+    printTwo5:
+        jal is_two
+        j continuar5
+
+    printEspaco5:
+        la $a0, espaco
+        li $v0, 4
+        syscall
+
+    continuar5:
 pos6:
-    li $t4, 6
     la $a0, coluna
     li $v0, 4
     syscall
     lw $t0, return6
     li $t1, 1
-    beq $t0, $t1, is_one
-    li $t2, 2
-    beq $t0, $t2, is_two
+    beq $t0, $t1, printOne6
 
-    la $a0, espaco
-    li $v0, 4
-    syscall
-    j pos7
+    li $t2, 2
+    beq $t0, $t2, printTwo6
+
+    j printEspaco6
+
+    printOne6:
+        jal is_one
+        j continuar6
+
+    printTwo6:
+        jal is_two
+        j continuar6
+
+    printEspaco6:
+        la $a0, espaco
+        li $v0, 4
+        syscall
+
+    continuar6:
 pos7:
-    li $t4, 7
     la $a0, newline
     li $v0, 4
     syscall
@@ -122,47 +205,87 @@ pos7:
     syscall
     lw $t0, return7
     li $t1, 1
-    beq $t0, $t1, is_one
-    li $t2, 2
-    beq $t0, $t2, is_two
+    beq $t0, $t1, printOne7
 
-    la $a0, espaco
-    li $v0, 4
-    syscall
-    j pos8
+    li $t2, 2
+    beq $t0, $t2, printTwo7
+
+    j printEspaco7
+
+    printOne7:
+        jal is_one
+        j continuar7
+
+    printTwo7:
+        jal is_two
+        j continuar7
+
+    printEspaco7:
+        la $a0, espaco
+        li $v0, 4
+        syscall
+
+    continuar7:
 pos8:
-    li $t4, 8
     la $a0, coluna
     li $v0, 4
     syscall
     lw $t0, return8
     li $t1, 1
-    beq $t0, $t1, is_one
-    li $t2, 2
-    beq $t0, $t2, is_two
+    beq $t0, $t1, printOne8
 
-    la $a0, espaco
-    li $v0, 4
-    syscall
-    j pos9
+    li $t2, 2
+    beq $t0, $t2, printTwo8
+
+    j printEspaco8
+
+    printOne8:
+        jal is_one
+        j continuar8
+
+    printTwo8:
+        jal is_two
+        j continuar8
+
+    printEspaco8:
+        la $a0, espaco
+        li $v0, 4
+        syscall
+
+    continuar8:
 pos9:
-    li $t4, 9
     la $a0, coluna
     li $v0, 4
     syscall
     lw $t0, return9
 
     li $t1, 1
-    beq $t0, $t1, is_one
-    li $t2, 2
-    beq $t0, $t2, is_two
+    beq $t0, $t1, printOne9
 
-    la $a0, espaco
-    li $v0, 4
-    syscall
-    j game_loop
+    li $t2, 2
+    beq $t0, $t2, printTwo9
+
+    j printEspaco9
+
+    printOne9:
+        jal is_one
+        j continuar9
+
+    printTwo9:
+        jal is_two
+        j continuar9
+
+    printEspaco9:
+        la $a0, espaco
+        li $v0, 4
+        syscall
+
+    continuar9:
+
+jal verificarVitoria
 
 game_loop:
+
     la $a0, newline
     li $v0, 4
     syscall
@@ -203,8 +326,11 @@ game_loop:
 r1:
     lw $t7, return1
     bnez $t7, mov_invalido
+
     lw $t5, turno
+
     andi $t1, $t5, 1 #$t1 will be 2 if it's odd, 1 if it's even
+
     li $t0, 1
     add $t1, $t1, $t0
     sw $t1, return1 
@@ -313,48 +439,316 @@ is_one:
     la $a0, X
     li $v0, 4
     syscall
-    li $t5, 1        # Load the value 1 into $t5
-    beq $t4, $t5, pos2  # Branch to "is_equal" if $t4 == $t5
-    li $t5, 2        # Load the value 1 into $t5
-    beq $t4, $t5, pos3  # Branch to "is_equal" if $t4 == $t5
-    li $t5, 3        # Load the value 1 into $t5
-    beq $t4, $t5, pos4  # Branch to "is_equal" if $t4 == $t5
-    li $t5, 4        # Load the value 1 into $t5
-    beq $t4, $t5, pos5  # Branch to "is_equal" if $t4 == $t5
-    li $t5, 5        # Load the value 1 into $t5
-    beq $t4, $t5, pos6  # Branch to "is_equal" if $t4 == $t5
-    li $t5, 6        # Load the value 1 into $t5
-    beq $t4, $t5, pos7  # Branch to "is_equal" if $t4 == $t5
-    li $t5, 7        # Load the value 1 into $t5
-    beq $t4, $t5, pos8  # Branch to "is_equal" if $t4 == $t5
-    li $t5, 8        # Load the value 1 into $t5
-    beq $t4, $t5, pos9  # Branch to "is_equal" if $t4 == $t5
-    li $t5, 9        # Load the value 1 into $t5
-    beq $t4, $t5, game_loop  # Branch to "is_equal" if $t4 == $t5
+    jr $ra
 is_two:
     la $a0, O
     li $v0, 4
     syscall
-    li $t5, 1        # Load the value 1 into $t5
-    beq $t4, $t5, pos2  # Branch to "is_equal" if $t4 == $t5
-    li $t5, 2        # Load the value 1 into $t5
-    beq $t4, $t5, pos3  # Branch to "is_equal" if $t4 == $t5
-    li $t5, 3        # Load the value 1 into $t5
-    beq $t4, $t5, pos4  # Branch to "is_equal" if $t4 == $t5
-    li $t5, 4        # Load the value 1 into $t5
-    beq $t4, $t5, pos5  # Branch to "is_equal" if $t4 == $t5
-    li $t5, 5        # Load the value 1 into $t5
-    beq $t4, $t5, pos6  # Branch to "is_equal" if $t4 == $t5
-    li $t5, 6        # Load the value 1 into $t5
-    beq $t4, $t5, pos7  # Branch to "is_equal" if $t4 == $t5
-    li $t5, 7        # Load the value 1 into $t5
-    beq $t4, $t5, pos8  # Branch to "is_equal" if $t4 == $t5
-    li $t5, 8        # Load the value 1 into $t5
-    beq $t4, $t5, pos9  # Branch to "is_equal" if $t4 == $t5
-    li $t5, 9        # Load the value 1 into $t5
-    beq $t4, $t5, game_loop  # Branch to "is_equal" if $t4 == $t5
+    jr $ra
 mov_invalido:
     la $a0, invalido
     li $v0, 4
     syscall
     j game_loop
+
+verificarVitoria:
+
+    # Verificação jogador 1
+    jal caso1O
+    jal caso1X
+    jal caso2O
+    jal caso2X
+    jal caso3O
+    jal caso3X
+    jal caso4O
+    jal caso4X
+    jal caso5O
+    jal caso5X
+    jal caso6O
+    jal caso6X
+    jal caso7O
+    jal caso7X
+    jal caso8O
+    jal caso8X
+    jal empate
+
+    # erificar X
+    caso1O:
+        c1l1:
+            lw $t7, return1
+            beq $t7, 1, c2l1
+            jr $ra
+        c2l1:
+            lw $t7, return2
+            beq $t7, 1, c3l1
+            jr $ra
+        c3l1:
+            lw $t7, return3
+            beq $t7, 1, vitoria1
+            jr $ra
+    caso1X:
+        c1l1_2:
+            lw $t7, return1
+            beq $t7, 2, c2l1_2
+            jr $ra
+        c2l1_2:
+            lw $t7, return2
+            beq $t7, 2, c3l1_2
+            jr $ra
+        c3l1_2:
+            lw $t7, return3
+            beq $t7, 2, vitoria2
+            jr $ra
+    caso2O:
+        c1l1_3:
+            lw $t7, return1
+            beq $t7, 1, c1l2_3
+            jr $ra
+        c1l2_3:
+            lw $t7, return4
+            beq $t7, 1, c1l3_3
+            jr $ra
+        c1l3_3:
+            lw $t7, return7
+            beq $t7, 1, vitoria2
+            jr $ra
+    caso2X:
+        c1l1_4:
+            lw $t7, return1
+            beq $t7, 2, c1l2_4
+            jr $ra
+        c1l2_4:
+            lw $t7, return4
+            beq $t7, 2, c1l3_4
+            jr $ra
+        c1l3_4:
+            lw $t7, return7
+            beq $t7, 2, vitoria2
+            jr $ra
+    caso3O:
+        c1l1_5:
+            lw $t7, return1
+            beq $t7, 1, c2l2_5
+            jr $ra
+        c2l2_5:
+            lw $t7, return5
+            beq $t7, 1, c3l3_5
+            jr $ra
+        c3l3_5:
+            lw $t7, return9
+            beq $t7, 1, vitoria1
+            jr $ra
+    caso3X:
+        c1l1_6:
+            lw $t7, return1
+            beq $t7, 2, c2l2_6
+            jr $ra
+        c2l2_6:
+            lw $t7, return5
+            beq $t7, 2, c3l3_6
+            jr $ra
+        c3l3_6:
+            lw $t7, return9
+            beq $t7, 2, vitoria2
+            jr $ra
+    caso4O:
+        c2l1_7:
+            lw $t7, return2
+            beq $t7, 1, c2l2_7
+            jr $ra
+        c2l2_7:
+            lw $t7, return5
+            beq $t7, 1, c3l2_7
+            jr $ra
+        c3l2_7:
+            lw $t7, return8
+            beq $t7, 1, vitoria1
+            jr $ra
+    caso4X:
+        c2l1_8:
+            lw $t7, return2
+            beq $t7, 2, c2l2_8
+            jr $ra
+        c2l2_8:
+            lw $t7, return5
+            beq $t7, 2, c3l2_8
+            jr $ra
+        c3l2_8:
+            lw $t7, return8
+            beq $t7, 2, vitoria2
+            jr $ra
+    caso5O:
+        c3l1_9:
+            lw $t7, return3
+            beq $t7, 1, c3l2_9
+            jr $ra
+        c3l2_9:
+            lw $t7, return6
+            beq $t7, 1, c3l3_9
+            jr $ra
+        c3l3_9:
+            lw $t7, return9
+            beq $t7, 1, vitoria1
+            jr $ra
+    caso5X:
+        c3l1_0:
+            lw $t7, return3
+            beq $t7, 2, c3l2_0
+            jr $ra
+        c3l2_0:
+            lw $t7, return6
+            beq $t7, 2, c3l3_0
+            jr $ra
+        c3l3_0:
+            lw $t7, return9
+            beq $t7, 2, vitoria2
+            jr $ra
+    caso6O:
+        c3l1_11:
+            lw $t7, return3
+            beq $t7, 1,  c2l2_11
+            jr $ra
+        c2l2_11:
+            lw $t7, return5
+            beq $t7, 1, c1l3_11
+            jr $ra
+        c1l3_11:
+            lw $t7, return7
+            beq $t7, 1, vitoria1
+            jr $ra
+    caso6X:
+        c3l1_22:
+            lw $t7, return3
+            beq $t7, 2, c2l2_22
+            jr $ra
+        c2l2_22:
+            lw $t7, return5
+            beq $t7, 2, c1l3_22
+            jr $ra
+        c1l3_22:
+            lw $t7, return7
+            beq $t7, 2, vitoria2
+            jr $ra
+    caso7O:
+        c1l2_33:
+            lw $t7, return4
+            beq $t7, 1, c2l2_33
+            jr $ra
+        c2l2_33:
+            lw $t7, return5
+            beq $t7, 1, c3l2_33
+            jr $ra
+        c3l2_33:
+            lw $t7, return6
+            beq $t7, 1, vitoria1
+            jr $ra
+    caso7X:
+        c1l2_44:
+            lw $t7, return4
+            beq $t7, 2, c2l2_44
+            jr $ra
+        c2l2_44:
+            lw $t7, return5
+            beq $t7, 2, c3l2_44
+            jr $ra
+        c3l2_44:
+            lw $t7, return6
+            beq $t7, 2, vitoria2
+            jr $ra
+    caso8O:
+        c1l3_55:
+            lw $t7, return7
+            beq $t7, 1, c2l3_55
+            jr $ra
+        c2l3_55:
+            lw $t7, return8
+            beq $t7, 1, c3l3_55
+            jr $ra
+        c3l3_55:
+            lw $t7, return9
+            beq $t7, 1, vitoria1
+            jr $ra
+    caso8X:
+        c1l3_66:
+            lw $t7, return7
+            beq $t7, 2, c2l3_66
+            jr $ra
+        c2l3_66:
+            lw $t7, return8
+            beq $t7, 2, c3l3_66
+            jr $ra
+        c3l3_66:
+            lw $t7, return9
+            beq $t7, 2, vitoria2
+            jr $ra
+
+
+    vitoria1:
+    la $a0, vitoria1_prompt
+    li $v0, 4
+    syscall
+    j exit
+    vitoria2:
+    la $a0, vitoria2_prompt
+    li $v0, 4
+    syscall
+    j exit
+
+empate:
+    jal casoe
+    j game_loop
+
+    casoe:
+        e1:
+            lw $t7, return1
+            beq $t7, 1, e2
+            beq $t7, 2, e2
+            jr $ra
+        e2:
+            lw $t7, return2
+            beq $t7, 1, e3
+            beq $t7, 2, e3
+            jr $ra
+        e3:
+            lw $t7, return3
+            beq $t7, 1, e4
+            beq $t7, 2, e4
+            jr $ra
+        e4:
+            lw $t7, return4
+            beq $t7, 1, e5
+            beq $t7, 2, e5
+            jr $ra
+        e5:
+            lw $t7, return5
+            beq $t7, 1, e6
+            beq $t7, 2, e6
+            jr $ra
+        e6:
+            lw $t7, return6
+            beq $t7, 1, e7
+            beq $t7, 2, e7
+            jr $ra
+        e7:
+            lw $t7, return7
+            beq $t7, 1, e8
+            beq $t7, 2, e8
+            jr $ra
+        e8:
+            lw $t7, return8
+            beq $t7, 1, e9
+            beq $t7, 2, e9
+            jr $ra
+        e9:
+            lw $t7, return9
+            beq $t7, 1, efinal
+            beq $t7, 2, efinal
+            jr $ra
+    efinal:
+        la $a0, empate_prompt
+        li $v0, 4
+        syscall
+        j exit
+exit:
+  li    $v0, 10                       # Finalizar aplicacao
+  syscall
